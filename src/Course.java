7f596134e965cfee06ef8b92d694fdb129b80f59
AlissaStorm <Alissaalidib@hotmail.com>
2024-12-20 15:59:57 +0100

    First commit

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Course {
+
+    private String name;
+
+    public Course(String name){
+        this.name = name;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
+
+    private Course course;
+    private int[] grade = {-3,0,2,4,7,10,12};
+    private String name;
+    private List<Course> courses;
+    private List<Integer> grades;
+
+    public Student(String name){
+        this.name = name;
+        this.courses = new ArrayList<>();
+        this.grades = new ArrayList<>();
+    }
+
+    public void addCourse(Course course, int grade){
+
+    }
 }
